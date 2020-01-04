44e2797d7a713e4adbb74b7ea115c9f6ce4e1242
Victor Tran <victor.n.tran@outlook.com>
2020-01-03 21:12:59 -0500

    import server

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const server = require('./server.js');
+const app = require('./server.js');
 
 const PORT = process.env.PORT || 4000;
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
 });
